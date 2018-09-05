36bbb1dc020ca2cc09760e6b04fb5ea1a8dd6c6d
petr <glinkap@gmail.com>
2018-09-05 14:58:08 +0300

    rrr

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -5,8 +5,16 @@ import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
 
 // const sagaMiddleware = createSagaMiddleware();
-const compMware = compose(applyMiddleware(thunk), /*applyMiddleware(sagaMiddleware),*/ window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-export default createStore(rootReducer, compMware);
+	console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+let finalStore;
+if (process.env.NODE_ENV === 'production') {
+	finalStore = createStore(rootReducer, applyMiddleware(thunk));
+} else {
+	const compMware = compose(applyMiddleware(thunk), /*applyMiddleware(sagaMiddleware),*/ window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+	finalStore = createStore(rootReducer, compMware);
+}
+export default finalStore;
+
 // sagaMiddleware.run(mySaga);
 // const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 // const store = createStoreWithMiddleware(reducer, 
