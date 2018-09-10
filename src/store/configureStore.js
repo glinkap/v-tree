d90fe0b95781d3a7d4e6a6a5d5da95e9154dd24a
Petr Glinka <glinkap@gmail.com>
2018-09-10 22:01:13 +0300

    devtools replace

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,6 @@
 import rootReducer from '../reducers/index'
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 // import createSagaMiddleware from 'redux-saga';
 // import mySaga from '../sagas/saga';
 import { createStore, applyMiddleware, compose } from 'redux';
@@ -7,9 +8,11 @@ import { createStore, applyMiddleware, compose } from 'redux';
 // const sagaMiddleware = createSagaMiddleware();
 let finalStore;
 if (process.env.NODE_ENV === 'production') {
-	finalStore = createStore(rootReducer, applyMiddleware(thunk));
+	// finalStore = createStore(rootReducer, applyMiddleware(thunk));
+	finalStore = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 } else {
-	const compMware = compose(applyMiddleware(thunk), /*applyMiddleware(sagaMiddleware),*/ window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+	// const compMware = compose(applyMiddleware(thunk), /*applyMiddleware(sagaMiddleware),*/ window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+	const compMware = composeWithDevTools(applyMiddleware(thunk));
 	finalStore = createStore(rootReducer, compMware);
 }
 export default finalStore;
